2b10ef15275e49f36b388833384d93df342a8c84
Hector Valls Mira <hectorvallsmira2003@gmail.com>
2022-03-18 19:09:23 +0100

    [NEW] JavaDoc creado de nuevo

--- src/main/java/es/cipfpbatoi/ed/u6p2/App.java
+++ src/main/java/es/cipfpbatoi/ed/u6p2/App.java
@@ -6,6 +6,11 @@ package es.cipfpbatoi.ed.u6p2;
  */
 public class App 
 {
+	
+	/**
+	 * Metodo que realizara la implentacion de la calculadora   
+	 * @param args parametro por defecto del main de java
+	 */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );

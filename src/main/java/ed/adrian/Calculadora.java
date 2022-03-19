0f5eeaf11353dd42add278c05d767ce686d3c669
Adrian Pascual Silvestre <adrianpascua142003@gmail.com>
2022-03-20 00:53:02 +0100

    [MOD] Anyadits i refactorits 2 primers metodes

--- src/main/java/ed/adrian/Calculadora.java
+++ src/main/java/ed/adrian/Calculadora.java
@@ -1,7 +1,8 @@
 package ed.adrian;
 
 /**
- * Serveix per a calcular operacions matemàtiques dos numeros pasats com a parametres
+ * Serveix per a calcular operacions matemàtiques dos numeros pasats com a
+ * parametres
  * <p>
  * Aquesta clase ens ofereix mètodes per a poder fer operacions matemàtiques com
  * sumes, restes, multiplicacions i divisions.
@@ -14,7 +15,7 @@ package ed.adrian;
  */
 
 public class Calculadora {
-	
+
 	/**
 	 * Fa una suma entre dos numeros pasats com a parametres
 	 * 
@@ -22,11 +23,11 @@ public class Calculadora {
 	 * @param b (altre numero per a sumar)
 	 * @return (la suma del primer element mes el segon element)
 	 */
-	
+
 	public int suma(int a, int b) {
 		return a + b;
 	}
-	
+
 	/**
 	 * Fa una resta entre dos numeros pasats com a parametres
 	 * 
@@ -34,11 +35,11 @@ public class Calculadora {
 	 * @param b (el segundo numero per a restar)
 	 * @return (retorna la resta del primer element menys el segon element)
 	 */
-	
+
 	public int resta(int a, int b) {
 		return a - b;
 	}
-	
+
 	/**
 	 * Fa una multiplicacio entre dos numeros pasats com a parametres
 	 * 
@@ -46,11 +47,11 @@ public class Calculadora {
 	 * @param b (el segon factor per a multiplicar)
 	 * @return (retorna la multipliació del primer element per el segon element)
 	 */
-	
-	public int multiplica(int a, int b){
-		return a*b;
+
+	public int multiplica(int a, int b) {
+		return a * b;
 	}
-	
+
 	/**
 	 * Fa una divisió entre dos numeros pasats com a parametres
 	 * 
@@ -59,9 +60,69 @@ public class Calculadora {
 	 * @return (retorna el quocient del divident entre el divisor)
 	 * @throws ArithmeticException (quan es divideix un numero entre 0)
 	 */
-	
+
 	public int diveix(int a, int b) {
-		return a/b;
+		return a / b;
+	}
+	
+	/**
+	 * Eleva la un numero a un nombre de voltes
+	 * 
+	 * @param potencia (numero al qual nem a elevar)
+	 * @param exponent (voltes que volem elevar la potencia)
+	 * @return (retorna la potencia multiplicada per si mateixa el numero de voltes pasades com exponent)
+	 */
+
+	public int elevar(int potencia, int exponent) {
+
+		int total = 1;
+
+		for (int i = 0; i < exponent; i++) {
+			total = total * potencia;
+		}
+
+		return total;
+
+	}
+	
+	/**
+	 * Aquesta funció ens diu si un numero pasat com a parametre es parell
+	 * 
+	 * @param numero (numero el qual volem comprovar si es parell)
+	 * @return (retorna true si es parell y false si es senar)
+	 */
+
+	public boolean isParell(int numero) {
+
+		boolean isPrimo;
+
+		if (numero % 2 == 0) {
+			isPrimo = true;
+		} else {
+			isPrimo = false;
+		}
+
+		return isPrimo;
+
+	}
+
+	public String oper3(int a){
+
+	    String r = "";
+	    int v = a;
+	    int v1;
+
+	    while ( v > 0) {
+
+	        v1 = v % 2;
+
+	        r = v1 + r;
+
+	        v = v / 2;
+
+	    }
+	    
+	    return r;
 	}
 	
 }
---
+++ src/main/java/ed/adrian/Test.java
@@ -0,0 +1,13 @@
+package ed.adrian;
+
+public class Test {
+
+	public static void main(String[] args) {
+	
+		Calculadora calculadora = new Calculadora();
+		
+		System.out.println(calculadora.oper3(10));
+		
+	}
+
+}

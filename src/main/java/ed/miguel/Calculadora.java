a863c1690516ddad8df782f968f63c840bc3ee3b
Miguel Castro <miguelcastrojulia@hotmail.com>
2022-03-19 12:24:23 +0100

    [ADD] realizado ejercicio3 y añadido la carpeta DOC,de la documantación Javadoc

--- src/main/java/ed/miguel/Calculadora.java
+++ src/main/java/ed/miguel/Calculadora.java
@@ -16,39 +16,40 @@ package ed.miguel;
 
 public class Calculadora {
 	
-	
-	public int suma(int a, int b) {
-	/**
+	 /**
 	 * 
 	 * @param a Primera variable int para realizar la suma
 	 * @param b Segunda variable int para realizar la suma
 	 * @return El valor int de la suma de las dos variables
 	 */
+	public int suma(int a, int b) {
+	
 		return a + b;
 	}
 	
-	public int resta(int a, int b) {
-		/**
+	/**
 		 * 
 		 * @param a Primera variable int para realizar la resta
 		 * @param b Segunda variable int para realizar la resta
 		 * @return El valor int de la resta de las dos variables
 		 */
+	public int resta(int a, int b) {
+		
 		return a - b;
 	}
 	
-	public int multiplica(int a, int b){
-		/**
+	/**
 		 * 
 		 * @param a Primera variable int para realizar la multiplicación
 		 * @param b Segunda variable int para realizar la multiplicación
 		 * @return El valor int de la multiplicación de las dos variables
 		 */
+	public int multiplica(int a, int b){
+		
 		return a*b;
 	}
 	
-	public int diveix(int a, int b) {
-		/**
+	/**
 		 * 
 		 * @param a Primera variable int, es el dividendo para realizar la division
 		 * @param b Segunda variable int  es el divisor para realizar la division
@@ -56,11 +57,13 @@ public class Calculadora {
 		 * algún caso diera el resultado decimales, solo mostrará el valor entero.
 		 * 
 		 */
+	public int diveix(int a, int b) {
+			
+		return a/b;
 		/**
 		 * @throws AritmhmeticException by zero
 		 * El divisor no puede ser cero
 		 */
-		return a/b;
 	}
 	
 }

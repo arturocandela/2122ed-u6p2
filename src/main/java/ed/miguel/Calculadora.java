8c656d8fa7504347e046a8830c46474ce576ae06
Miguel Castro <miguelcastrojulia@hotmail.com>
2022-03-19 14:25:31 +0100

    [ADD] realizado ejercicio6 y modificado la carpeta DOC,de la documantación Javadoc

--- src/main/java/ed/miguel/Calculadora.java
+++ src/main/java/ed/miguel/Calculadora.java
@@ -18,52 +18,112 @@ public class Calculadora {
 	
 	 /**
 	 * 
-	 * @param a Primera variable int para realizar la suma
-	 * @param b Segunda variable int para realizar la suma
+	 * @param numero1 Primera variable int para realizar la suma
+	 * @param numero2 Segunda variable int para realizar la suma
 	 * @return El valor int de la suma de las dos variables
 	 */
-	public int suma(int a, int b) {
+	public int suma(int numero1, int numero2) {
 	
-		return a + b;
+		return numero1 + numero2;
 	}
 	
 	/**
 		 * 
-		 * @param a Primera variable int para realizar la resta
-		 * @param b Segunda variable int para realizar la resta
+		 * @param numero1 Primera variable int para realizar la resta
+		 * @param numero2 Segunda variable int para realizar la resta
 		 * @return El valor int de la resta de las dos variables
 		 */
-	public int resta(int a, int b) {
+	public int resta(int numero1, int numero2) { 
 		
-		return a - b;
+		return numero1 - numero2; 
 	}
 	
 	/**
 		 * 
-		 * @param a Primera variable int para realizar la multiplicación
-		 * @param b Segunda variable int para realizar la multiplicación
+		 * @param multiplicando Primera variable int para realizar la multiplicación
+		 * @param multiplicador Segunda variable int para realizar la multiplicación
 		 * @return El valor int de la multiplicación de las dos variables
 		 */
-	public int multiplica(int a, int b){
+	public int multiplica(int multiplicando, int multiplicador){ 
 		
-		return a*b;
+		return multiplicando*multiplicador; 
 	}
-	
+	 
 	/**
 		 * 
-		 * @param a Primera variable int, es el dividendo para realizar la division
-		 * @param b Segunda variable int  es el divisor para realizar la division
+		 * @param dividendo Primera variable int, es el dividendo para realizar la division
+		 * @param divisor Segunda variable int  es el divisor para realizar la division
 		 * @return El valor int de la division de las dos variables, y si por 
 		 * algún caso diera el resultado decimales, solo mostrará el valor entero.
 		 * 
 		 */
-	public int diveix(int a, int b) {
+	public int diveix(int dividendo, int divisor) {
 			
-		return a/b;
+		return dividendo/divisor;
 		/**
 		 * @throws AritmhmeticException by zero
 		 * El divisor no puede ser cero
 		 */
+	}
+	
+	/**
+	 * 
+	 * @param base Entero para realizar la potencia
+	 * @param exponente Entero con las veces que se multiplica la base
+	 * @return retorna el resultado de la multiplicación de la base por si misma, tantas veces sea el exponente.
+	 */
+	
+	public int potencia ( int base , int exponente ){
+
+
+		 int resultadoPotencia = 1;
+
+		 for ( int i = 0; i < exponente ; i++ ){
+		       resultadoPotencia = resultadoPotencia * base;
+	    }
+
+		 return resultadoPotencia;
+
+	}
+	/**
+	 * 
+	 * @param numero variable int para saber si es par, al ser dividido por dos y su resto es cero.
+	 * @return boleano: true, si es par y false, si no lo es
+	 */
+
+	public boolean esPar ( int numero ){
+
+	    boolean resultado;
+
+	    if ( numero % 2 == 0) {
+	        resultado = true;
+	    } else {
+	        resultado = false;
+	    }
+
+	    return resultado;
+
+	}
+/**
+ * 
+ * @param numero Introducimos entero, para saber cual es en binario
+ * @return nos retorna un String con los restos de las operaciones y así nos devuelve su binario.
+ */
+	public String enteroABinario(int numero){
+
+	    String binario = "";
+	    int resto;
+
+	    while ( numero > 0) {
+
+	        resto = numero % 2;
+
+	        binario = resto + binario;
+
+	        numero = numero / 2;
+
+	    }
+	    return binario;
 	}
 	
 }
--- src/main/java/ed/miguel/TestCalculador.java
+++ src/main/java/ed/miguel/TestCalculador.java
@@ -9,6 +9,9 @@ public class TestCalculador {
 		System.out.println(calculadora.resta(45,25));
 		System.out.println(calculadora.multiplica(45,25));
 		System.out.println(calculadora.diveix(25,5));
+		System.out.println(calculadora.potencia(2, 2));
+		System.out.println(calculadora.esPar(28));
+		System.out.println(calculadora.enteroABinario(10));
 	}
 
 }

f61a5a451cecabb79d51fc6137ca7a74ec856e3a
Sergio PM <spayam82@gmail.com>
2022-03-19 11:05:47 +0100

    [MOD] A2 Documentar Calculadora

--- src/main/java/ed/sergi/Calculadora.java
+++ src/main/java/ed/sergi/Calculadora.java
@@ -1,19 +1,56 @@
 package ed.sergi;
 
+/**
+ * Fa càlcus amb els paràmetres rebuts
+ * <p>
+ * Aquesta classe actua com a calculadora i permet retornar el
+ *  resultat dels paràmetres rebuts a diversos tipus de càlculs
+ * </p>
+ * @author Sergi Payà
+ * @version 2.5
+ * @since 1.9, juny de 2019
+ *
+ */
 public class Calculadora {
 	
+	/**
+	 * 
+	 * Reb 2 paràmetres i els suma
+	 * @param a primer sumand
+	 * @param b segon sumand
+	 * @return Retorna la suma de a i b
+	 */
 	public int suma(int a, int b) {
 		return a + b;
 	}
 	
+	/**
+	 * Reb 2 parametres i els resta
+	 * @param a valor inicial
+	 * @param b valor a restar
+	 * @return Retorna la resta de a - b
+	 */
 	public int resta(int a, int b) {
 		return a - b;
 	}
 	
+	/**
+	 * Reb 2 parametres i els multiplica
+	 * @param a multiplicand
+	 * @param b multiplicador
+	 * @return Retorna la multiplicacio de a * b
+	 */
 	public int multiplica(int a, int b){
 		return a*b;
 	}
 	
+	/**
+	 * Reb dos parametres y els divideix
+	 * @param a dividend
+	 * @param b divisor
+	 * @return Retorna el resultat de a / b
+	 * @exception ArithmeticException Si el divisor es 0 no es podra fer la divisio
+	 */
 	public int diveix(int a, int b) {
 		return a/b;
 	}

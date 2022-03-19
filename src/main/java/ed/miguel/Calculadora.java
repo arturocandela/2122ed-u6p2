ee646c512c47ea0acb099083c291801149ec0ce0
Miguel Castro <miguelcastrojulia@hotmail.com>
2022-03-19 11:49:47 +0100

    [ADD] realizado ejercicio2 y añadido la clase TestCalculadora, para realizar tests

--- src/main/java/ed/miguel/Calculadora.java
+++ src/main/java/ed/miguel/Calculadora.java
@@ -1,20 +1,65 @@
 package ed.miguel;
+/**
+ * Clase para realizar operaciones matematicas
+ */
+
+/**
+ * Clase con metodos aritméticos, para realizar operaciones matemáticas como:
+ * sumar, restar, multiplicar y dividir.
+ * 
+ * 
+ * @author Miguel Castro Juliá
+ * @version 2.5
+ * @since 1.9 desde junio del 2019
+ *
+ */
 
 public class Calculadora {
 	
+	
 	public int suma(int a, int b) {
+	/**
+	 * 
+	 * @param a Primera variable int para realizar la suma
+	 * @param b Segunda variable int para realizar la suma
+	 * @return El valor int de la suma de las dos variables
+	 */
 		return a + b;
 	}
 	
 	public int resta(int a, int b) {
+		/**
+		 * 
+		 * @param a Primera variable int para realizar la resta
+		 * @param b Segunda variable int para realizar la resta
+		 * @return El valor int de la resta de las dos variables
+		 */
 		return a - b;
 	}
 	
 	public int multiplica(int a, int b){
+		/**
+		 * 
+		 * @param a Primera variable int para realizar la multiplicación
+		 * @param b Segunda variable int para realizar la multiplicación
+		 * @return El valor int de la multiplicación de las dos variables
+		 */
 		return a*b;
 	}
 	
 	public int diveix(int a, int b) {
+		/**
+		 * 
+		 * @param a Primera variable int, es el dividendo para realizar la division
+		 * @param b Segunda variable int  es el divisor para realizar la division
+		 * @return El valor int de la division de las dos variables, y si por 
+		 * algún caso diera el resultado decimales, solo mostrará el valor entero.
+		 * 
+		 */
+		/**
+		 * @throws AritmhmeticException by zero
+		 * El divisor no puede ser cero
+		 */
 		return a/b;
 	}
 	
---
+++ src/main/java/ed/miguel/TestCalculador.java
@@ -0,0 +1,14 @@
+package ed.miguel;
+
+public class TestCalculador {
+
+	public static void main(String[] args) {
+		
+		Calculadora calculadora = new Calculadora();
+		System.out.println(calculadora.suma(45,25));
+		System.out.println(calculadora.resta(45,25));
+		System.out.println(calculadora.multiplica(45,25));
+		System.out.println(calculadora.diveix(25,5));
+	}
+
+}

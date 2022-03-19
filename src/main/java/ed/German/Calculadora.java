c0db46ecb638bbdd75ff38da6c350dc7a7cdc94f
Germán Juan <wambows@gmail.com>
2022-03-19 13:10:00 +0100

    Documentacio de la classe feta

--- src/main/java/ed/German/Calculadora.java
+++ src/main/java/ed/German/Calculadora.java
@@ -1,4 +1,14 @@
-package ed.arturo;
+/**
+ * Classe Calculadora -> Realitza opeacions matemàtiques
+ * 
+ * Aquesta clase té la utilitat de rebre 2 números, i depenent de l'operació sel.leccionada, retornarà
+ * el valor corrsponent amb l'operació.
+ * @author Germán
+ * @version 2.5
+ * @since 1.9 juny 2019
+ */
+
+package ed.German;
 
 public class Calculadora {
 	

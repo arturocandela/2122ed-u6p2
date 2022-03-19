1175000f514d7150c302b2cea8e3decc47fb53ff
Sergio PM <spayam82@gmail.com>
2022-03-19 12:43:16 +0100

    [MOD] A6 Afegides les funcions i comentades

--- src/main/java/ed/sergi/Calculadora.java
+++ src/main/java/ed/sergi/Calculadora.java
@@ -55,4 +55,67 @@ public class Calculadora {
 		return a/b;
 	}
 	
+	/**
+	 * Eleva un numero N vegades
+	 * @param num numero que elevar
+	 * @param pot potencia a la que s'eleva
+	 * @return resultat d'elevar num^pot
+	 */
+	public int potencia ( int num , int pot ){
+
+	    int total = 1;
+
+	    for ( int i = 0; i < pot ; i++ ){
+	        total = total * num;
+	    }
+
+	    return total; 
+
+	}
+
+	/**
+	 * Reb un numero i respon si es par o no
+	 * @param num numero a comprovar
+	 * @return retorna true si num es par i false si no ho es
+	 */
+	public boolean isPar ( int num ){
+
+	    boolean isPar;
+
+	    if ( num % 2 == 0) {
+	        isPar = true;
+	    } else {
+	        isPar = false;
+	    }
+
+	    return isPar;
+
+	}
+
+	/**
+	 * Obte un string amb una serie de restos del numero
+	 * @param num numero del que obtindre els restos
+	 * @return String amb una seqüencia de restos de tantes voltes com num es puga dividir entre 2
+	 */
+	public String cadenaRestos(int num){
+
+	    String resultat = "";
+	    int numero = num;
+	    int resto;
+
+	    while ( numero > 0) {
+
+	        resto = numero % 2;
+
+	        resultat = resto + resultat;
+
+	        numero = numero / 2;
+
+
+	    }
+	   
+	    return resultat;
+	    
+	}
+	
 }
---
+++ src/main/java/ed/sergi/TestCalculadora.java
@@ -0,0 +1,15 @@
+package ed.sergi;
+
+public class TestCalculadora {
+
+	public static void main( String[] args ) {
+		
+		Calculadora calculadora = new Calculadora();
+		
+		String restos = calculadora.cadenaRestos(5);
+		
+		System.out.println(restos);
+
+	}
+	
+}

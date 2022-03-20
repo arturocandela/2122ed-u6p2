283e0afc6ca54641c57c74c3719853a1884d174b
Carolina Torregrosa Nadal <carolatn@gmail.com>
2022-03-20 12:31:06 +0100

    [MOD] Actividad 2-1: Comentarios para la clase realizados

--- src/main/java/ed/carolina/Calculadora.java
+++ src/main/java/ed/carolina/Calculadora.java
@@ -1,3 +1,18 @@
+/**
+ * Clase que permite realizar operaciones matemáticas.
+ * 
+ * <p>
+ * La clase calculadora permite realizar una serie de operaciones pasando los operandos como
+ * parámetros y devolviendo el resultado de dicha operación que tendremos que guardar en 
+ * una variable para su posterior uso.
+ * Las operaciones disponibles son: suma, resta, multiplica y divide.
+ * </p>
+ * 
+ * @author Carolina Torregrosa Nadal
+ * @version 2.5 (Versión actual de la clase)
+ * @since 1.9 Junio 2019 (Versión desde la cual está disponible esta clase)
+ */
+
 package ed.carolina;
 
 public class Calculadora {

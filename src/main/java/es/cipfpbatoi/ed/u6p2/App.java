1ceb0445b815fbc6257e536adba3413cfb9d62a6
Carolina Torregrosa Nadal <carolatn@gmail.com>
2022-03-20 11:53:09 +0100

    [MOD] Modificado main para comprobar funcionamiento de calculadora

--- src/main/java/es/cipfpbatoi/ed/u6p2/App.java
+++ src/main/java/es/cipfpbatoi/ed/u6p2/App.java
@@ -1,5 +1,7 @@
 package es.cipfpbatoi.ed.u6p2;
 
+import ed.arturo.Calculadora;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculadora calculadora = new Calculadora();
+        
+        System.out.println("2+2=" + calculadora.suma(2, 2));
+        System.out.println("3-2=" + calculadora.resta(3, 2));
+        System.out.println("2*4=" + calculadora.multiplica(2, 4));
+        System.out.println("6/2=" + calculadora.diveix(6, 2));
+        
     }
 }

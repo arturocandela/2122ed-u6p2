972bb8dbad5015cf8c73746a6cafc7d2c7e82ec0
Carolina Torregrosa Nadal <carolatn@gmail.com>
2022-03-20 12:45:26 +0100

    [MOD] Actividad 2-2: Comentarios para los métodos realizados

--- src/main/java/ed/carolina/Calculadora.java
+++ src/main/java/ed/carolina/Calculadora.java
@@ -17,18 +17,53 @@ package ed.carolina;
 
 public class Calculadora {
 	
+	
+	/**
+	 * Suma dos operandos enteros y devuelve un entero con el resultado de la suma. Se permite
+	 * el uso de enteros negativos o positivos.
+	 * 
+	 * @param a Primer sumando.
+	 * @param b Segundo sumando.
+	 * @return Resultado de la suma de a y b
+	 */
 	public int suma(int a, int b) {
 		return a + b;
 	}
 	
+	/**
+	 * Resta dos operandos enteros y devuelve un entero con el resultado de la resta,
+	 * pudiendo ser un resultado negativo o positivo.
+	 * 
+	 * @param a Primer número de la resta (minuendo).
+	 * @param b Segundo número de la rest (sustraendo).
+	 * @return Resultado de la resta de a-b (diferencia).
+	 */
 	public int resta(int a, int b) {
 		return a - b;
 	}
 	
+	/**
+	 * Multiplica dos operandos enteros y devuelve un entero con el resultado de la operación,
+	 * pudiendo ser todos los números tanto positivos como negativos.
+	 * 
+	 * @param a Primer factor a multiplicar.
+	 * @param b Segundo factor a multiplicar.
+	 * @return Producto de a y b.
+	 */
 	public int multiplica(int a, int b){
 		return a*b;
 	}
 	
+	/**
+	 * Divide un entero entre otro, devolviendo otro entero y descartando los decimales o el
+	 * resto de dicha operación. En el caso de que la división sea entre 0, se lanzará una
+	 * excepción de tipo ArithmeticException.
+	 * 
+	 * @param a Primer número a dividir o dividendo
+	 * @param b Segundo número entre el que se divide o divisor
+	 * @return Cociente de a entre b
+	 * @throws ArithmeticException Se obtiene cuando el divisor es 0.
+	 */
 	public int diveix(int a, int b) {
 		return a/b;
 	}

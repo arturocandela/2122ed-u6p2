9df9c8c5cd1c24e1c3adf895d14efa53a13feb81
kessy7u7 <kessvan.one@gmail.com>
2022-03-20 17:37:51 +0100

    Activitat 6 - Documentació de tres nous mètodes

--- src/main/java/ed/kessvan/Calculadora.java
+++ src/main/java/ed/kessvan/Calculadora.java
@@ -16,8 +16,8 @@ public class Calculadora {
 	/**
 	 * Mètode per a sumar dues nombres enters
 	 * 
-	 * @param Primer nombre
-	 * @param Segon nombre
+	 * @param a Primer nombre
+	 * @param b Segon nombre
 	 * @return Suma dels sumants
 	 */
 	public int suma(int a, int b) {
@@ -27,8 +27,8 @@ public class Calculadora {
 	/**
 	 * Mètode per a restar dues nombres enters
 	 * 
-	 * @param Primer nombre
-	 * @param Segon nombre
+	 * @param a Primer nombre
+	 * @param b Segon nombre
 	 * @return El primer nombre menys el segon
 	 */
 	public int resta(int a, int b) {
@@ -38,8 +38,8 @@ public class Calculadora {
 	/**
 	 * Mètode per a multiplicar dues nombres enters
 	 * 
-	 * @param Primer multiplicant
-	 * @param Segon multiplicant
+	 * @param a Primer multiplicant
+	 * @param b Segon multiplicant
 	 * @return Producte dels multiplicants
 	 */
 	public int multiplica(int a, int b){
@@ -49,8 +49,8 @@ public class Calculadora {
 	/**
 	 * Mètode per a dividir dues nombre enters
 	 * 
-	 * @param Dividendo
-	 * @param Divisor
+	 * @param a Dividendo
+	 * @param b Divisor
 	 * @return Cociente
 	 * @throws ArithmeticException si el divisor es 0
 	 */
@@ -58,4 +58,48 @@ public class Calculadora {
 		return a/b;
 	}
 	
+	/**
+	 * Mètode que eleva un nombre n vegades
+	 * 
+	 * @param base Nombre que es multiplica
+	 * @param exponent Nombre de vegades que es multiplica la base
+	 * @return La base elevado al exponente
+	 */
+	public int exponer(int base , int exponent){
+	    int resultat = 1;
+	    for ( int i = 0; i < exponent ; i++ ){
+	        resultat = resultat * base;
+	    }
+	    return resultat; 
+	}
+
+	/**
+	 * Mètode que comprova si un nombre es parell
+	 * 
+	 * @param a Nombre a comprovar
+	 * @return true si el nombre es parell, false en cas contrari
+	 */
+	public boolean esParell(int a){
+	    return a % 2 == 0;
+	}
+
+	/**
+	 * Mètode que retorne el codi binari d'un nombre
+	 * 
+	 * @param a Nombre enter
+	 * @return Codi binari generat
+	 */
+	public String toBinary(int a){
+	    String codiBinari = "";
+	    int aux = a;
+	    int aux2;
+
+	    while (aux > 0) {
+	        aux2 = aux % 2;
+	        codiBinari += aux2;
+	        aux /= 2;
+	    }
+	    
+	    return codiBinari;	    
+	}
 }

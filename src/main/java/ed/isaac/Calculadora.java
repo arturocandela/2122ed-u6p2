12b1fe86100680a56ddf54eccc91f6b205391e55
Arturo Candela <acandela@cipfpbatoi.es>
2022-04-06 19:00:40 +0200

    Revision finalizada

--- src/main/java/ed/isaac/Calculadora.java
+++ src/main/java/ed/isaac/Calculadora.java
@@ -2,16 +2,20 @@ package ed.isaac;
 
 /**
  * Clase para realización de distintas operaciones con enteros pasados como argumentos.
- * <p>La clase calculadora tiene la finalidad de reaalizar los ejercicios planteados en la práctica 2 para trabajar un poco con la documentación del código de java. Sus principales métodos son para sumar, restar, multiplicar y dividir diferentes enteros que pasaremos como argumento al llamar al método.</p>
+ * <p>
+ * La clase calculadora tiene la finalidad de reaalizar los ejercicios planteados en la práctica 2 para trabajar un poco con la documentación del código de java. Sus principales métodos son para sumar, restar, multiplicar y dividir diferentes enteros que pasaremos como argumento al llamar al método.</p>
+ * 
  * @author isaac
  * @version 2.5
- * @version disponible desde la 1.9
+ * @since 1.9, junio de 2019
+ *
  */
 
 public class Calculadora {
 	
 	/**
 	 * Realizar una suma con los parametros pasados como argumento
+	 * 
 	 * @param a operando1
 	 * @param b operando2
 	 * @return El resultado de la suma de a + b
@@ -21,6 +25,7 @@ public class Calculadora {
 	}
 	/**
 	 * Realizar una resta con los parametros pasados como argumento
+	 * 
 	 * @param a operando1
 	 * @param b operando2
 	 * @return El resultado de la resta de a - b
@@ -42,7 +47,7 @@ public class Calculadora {
 	 * @param a operando1
 	 * @param b operando2
 	 * @return El resultado de la division de a / b
-	 * @exception al intentar dividir un número por 0
+	 * @exception ArithmeticException al intentar dividir un número por 0
 	 */
 	public int diveix(int a, int b) {
 		return a/b;

4d2fae4da8705fb08154fcf4bd9abf64cb622c77
Arturo Candela <acandela@cipfpbatoi.es>
2022-04-06 17:23:07 +0200

    Revisión finlizada

--- src/main/java/ed/diego/Calculadora.java
+++ src/main/java/ed/diego/Calculadora.java
@@ -1,21 +1,24 @@
 package ed.diego;
 
-/**Hola
+/**
  * Esto es una calculadora con diferentes opciones de calculo
+ * <p>
+ * La clase tiene diferentes métodos para realizar todas las opciones de
+ * cálculo. ( Más descripción, la larga )
+ * 
  * @author Diego Matarredona Sancho
  * @version 2.5
- * 
+ * @since 1.9, julio de 2019
  */
 public class Calculadora {
 	
 	/**
 	 * Suma el primer numero con el segundo
+	 * 
 	 * @param a primero numero
 	 * @param b segundo numero
-	 * @return el resultado de la suma
-	 * @exception java.lang.ArithmeticException Salta cuando hay un error de calculo
+	 * @return la suma de los dos números
 	 */
-	
 	public int suma(int a, int b) {
 		return a + b;
 	}
@@ -34,22 +37,22 @@ public class Calculadora {
 	
 	/**
 	 * Multiplica el primero numero por el segundo
+	 * 
 	 * @param a primero numero
 	 * @param b segundo numero
 	 * @return el resultado de la multiplicacion
-	 * @exception java.lang.ArithmeticException Salta cuando hay un error de calculo
 	 */
-	
 	public int multiplica(int a, int b){
 		return a*b;
 	}
 	
 	/**
 	 * Divide el primer numero entre el segundo
+	 * 
 	 * @param a primero numero
 	 * @param b segundo numero
-	 * @return el resultado de la division
-	 * @exception java.lang.ArithmeticException Salta si divides entre 0 
+	 * @return el resultado de la division 
+	 * @exception java.lang.ArithmeticException Salta cuando b es 0.
 	 */
 	
 	public int diveix(int a, int b) {
